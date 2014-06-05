b2670dfafab17bd1c32fe6708629b0ca9bd13cbb
Mikko Oksa <m3tacity@gmail.com>
2014-06-06 00:38:52 +0300

    Make DSN, db username and db passwords constants

--- init.php
+++ init.php
@@ -1,10 +1,23 @@
 <?php
 
+////////////////////////////////////
+// CHANGE THE VALUES TO YOUR OWN
+////////////////////////////////////
+define("DB_DSN", "mysql:dbname=some_table_name;host=localhost");
+define("DB_USERNAME", "your_db_username");
+define("DB_PASSWORD", "your_db_password");
+
+// Change these values if you want/need to..
+define("LINES_TABLE", "kuopio_buslines");
+define("ROUTES_TABLE", "kuopio_busline_routes");
+define("ROUTESTOPS_TABLE", "kuopio_busline_routestops");
+
+
 $pdo;
 try {
-	$pdo = new PDO('YOUR_DSN_HERE', 'USERNAME', 'PASSWORD');
+	$pdo = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
 } catch (PDOException $pdoex) {
-	die('ERROR: Could not connect: ' . $pdoex->getMessage());
+	die("ERROR: Could not connect: " . $pdoex->getMessage());
 }
 
 function dieWithInternalError($pdo) {
@@ -13,7 +26,8 @@ function dieWithInternalError($pdo) {
 }
 
 function jsonRemoveUnicodeSequences($struct) {
-	return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+	return preg_replace("/\\\\u([a-f0-9]{4})/e", 
+		"iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
 }
 
 ?>
